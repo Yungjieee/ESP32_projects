ad555fc786cee418e2711e792e8ea19182c75d82
Yungjieee <yungjieleex@gmail.com>
2025-06-13 22:16:33 +0800

    Update ultrasonic_sensor.ino

--- ultrasonic_sensor/ultrasonic_sensor.ino
+++ ultrasonic_sensor/ultrasonic_sensor.ino
@@ -9,8 +9,8 @@
   copies or substantial portions of the Software.
 *********/
 
-const int trigPin = 5;
-const int echoPin = 18;
+const int trigPin = 18;
+const int echoPin = 5;
 
 //define sound speed in cm/uS
 #define SOUND_SPEED 0.034
